487ee3f4d4b30674d36772d59804bc22858714bf
josem <josemeria93@gmail.com>
2017-04-28 18:10:44 -0700

    First commit, BankProject for C# class

---
+++ BankProject/Account.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject
+{
+    /// <summary>
+    /// This class represents a bank account.
+    /// Here you canm deposit/withdraw
+    /// </summary>
+    class Account
+    {
+        #region Variables
+
+        private static int lastAccountNumber = 0;
+
+        #endregion
+        #region Properties
+        /// <summary>
+        /// Email Address of the account number
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        public int AccountNumber { get; private set; }
+
+        public decimal Balance { get; private set; }
+
+        public string TypeOfAccount { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Account()
+        {
+          
+            AccountNumber = ++lastAccountNumber;
+
+        }
+        public Account(string emailAddress) : this()
+        {
+            EmailAddress = emailAddress;
+        }
+
+        public Account(string emailAddress, string typeOfAccount): this(emailAddress)
+        {
+            TypeOfAccount = typeOfAccount;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        /// <returns>The updated balance</returns>
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+        
+
+        public decimal Withdraw (decimal amount)
+
+        {
+            Balance -= amount;
+            return Balance;
+        }
+        #endregion
+    }
+}
---
+++ BankProject/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var account = Bank.CreateAccount("test@test.com", 100, "Checking");
+            //Console.WriteLine($"AccountNumber: {account.AccountNumber}, Type: {account.TypeOfAccount}, Balance: {account.Balance}, Email Address: {account.EmailAddress}");
+
+            //var account2 = Bank.CreateAccount("test2@test.com", 150, "Savings");
+
+            //Console.WriteLine($"AccountNumber: {account2.AccountNumber}, Type: {account2.TypeOfAccount}, Balance: {account2.Balance}, Email Address: {account2.EmailAddress}");
+
+            Console.WriteLine("***Welcome To My Bank***");
+
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Create an account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Print all accounts");
+                Console.Write("5. Please select a choice from the above: ");
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "0":
+                        Console.WriteLine("Thank you!");
+                        return;
+
+                    case "1":
+                        Console.Write("Email Address :");
+                        var emailAddress = Console.ReadLine();
+                        Console.Write("Type of Account :");
+                        var typeOfAccount = Console.ReadLine();
+                        Console.Write("Amount to deposit :");
+                        var amount = Convert.ToDecimal(Console.ReadLine());
+
+                        var account = Bank.CreateAccount(emailAddress, amount, typeOfAccount);
+                        Console.WriteLine($"AccountNumber: {account.AccountNumber}, Type: {account.TypeOfAccount}, Balance: {account.Balance: C}, Email Address: {account.EmailAddress}");
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
